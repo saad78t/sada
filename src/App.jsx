f75615e92d6fc0985533159b45d1e109db629f02
saadturky <saad.turky2000@gmail.com>
2025-05-01 15:25:48 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 60px;
+  font-weight: 30px;
+  background-color: #fcf;
+`;
+
+function App() {
+  return <H1>Sada</H1>;
+}
+
+export default App;
