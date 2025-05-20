d890600a0879655f0d97d32588839a793dcdfe97
saadturky <saad.turky2000@gmail.com>
2025-05-20 21:39:41 +0300

    Week added to timeAgo function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -9,19 +9,28 @@ export function timeAgo(date) {
   const seconds = Math.floor((new Date() - new Date(date)) / 1000);
 
   let interval = Math.floor(seconds / 31536000);
-  if (interval > 1) return interval + " years ago";
+  if (interval >= 1)
+    return interval + (interval === 1 ? " year ago" : " years ago");
 
   interval = Math.floor(seconds / 2592000);
-  if (interval > 1) return interval + " months ago";
+  if (interval >= 1)
+    return interval + (interval === 1 ? " month ago" : " months ago");
+
+  interval = Math.floor(seconds / 604800);
+  if (interval >= 1)
+    return interval + (interval === 1 ? " week ago" : " weeks ago");
 
   interval = Math.floor(seconds / 86400);
-  if (interval > 1) return interval + " days ago";
+  if (interval >= 1)
+    return interval + (interval === 1 ? " day ago" : " days ago");
 
   interval = Math.floor(seconds / 3600);
-  if (interval > 1) return interval + " hours ago";
+  if (interval >= 1)
+    return interval + (interval === 1 ? " hour ago" : " hours ago");
 
   interval = Math.floor(seconds / 60);
-  if (interval > 1) return interval + " minutes ago";
+  if (interval >= 1)
+    return interval + (interval === 1 ? " minute ago" : " minutes ago");
 
   return "Just now";
 }
